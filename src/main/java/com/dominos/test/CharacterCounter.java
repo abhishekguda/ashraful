deb3522b33e7a282cf1bcc9f13fcb31c1115b77c
gudaa <abhishek.guda@dominos.com>
2022-10-28 10:50:11 -0400

    Adding Interview Questions for Ashraful

---
+++ src/main/java/com/dominos/test/CharacterCounter.java
@@ -0,0 +1,8 @@
+package com.dominos.test;
+
+public class CharacterCounter {
+  public int count(String keyword, char ch) {
+    //implement here
+    return 1;
+  }
+}
---
+++ src/main/java/com/dominos/test/CollectionAdder.java
@@ -0,0 +1,16 @@
+package com.dominos.test;
+
+/*
+Given an integer collection return a sum of its elements
+ */
+
+import java.util.List;
+
+public class CollectionAdder {
+
+    public int sum(List<Integer> numbers) {
+        int x = 0;
+        //implement here
+        return x;
+    }
+}
